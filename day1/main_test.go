a9234cc8ce9475b54fd6d1ff4f4358fdd7fdf449
Geoff Nin <geoff.nin@gmail.com>
2017-12-17 09:57:42 -0800

    day1 fixes and refactoring

--- day1/main_test.go
+++ day1/main_test.go
@@ -14,9 +14,29 @@ func TestExamplesReturnCorrectValue(t *testing.T) {
 	}
 
 	for _, test := range table {
-		response := sumFromString(test.input)
+		response := sumPart1(test.input)
 		if test.expected != response {
-			t.Errorf("Gave %v, got %v expected %v", test.input, response, test.expected)
+			t.Errorf("Gave %v, got %v but expected %v", test.input, response, test.expected)
+		}
+	}
+}
+
+func TestPart2ExamplesReturnCorrectValue(t *testing.T) {
+	table := []struct {
+		input    string
+		expected int
+	}{
+		{"1212", 6},
+		{"1221", 0},
+		{"123425", 4},
+		{"123123", 12},
+		{"12131415", 4},
+	}
+
+	for _, test := range table {
+		response := sumPart2(test.input)
+		if response != test.expected {
+			t.Errorf("Gave %v, got %v but expected %v", test.input, response, test.expected)
 		}
 	}
 }
